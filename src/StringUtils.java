d9e9361bd566b64f35f6e3724d732bba400dcfc2
Madalin Cotetiu <cotetiumadalin@gmail.com>
2018-12-20 17:33:24 +0200

    refactoring

--- src/StringUtils.java
+++ src/StringUtils.java
@@ -7,7 +7,7 @@ public final class StringUtils {
         return text.toLowerCase();
     }
 
-    public static ArrayList<Integer> findAllOccurences(String text, char currentElement){
+    public static ArrayList<Integer> findAllOccurrences(String text, char currentElement){
         int index = text.indexOf(currentElement);
         ArrayList<Integer> positions = new ArrayList<>();
         while (index >= 0) {
@@ -19,11 +19,11 @@ public final class StringUtils {
 
     public static HashMap<Character, ArrayList<Integer>> createMap(String text) {
 
-        ArrayList<Integer> positions = new ArrayList<>();
+        ArrayList<Integer> positions;
         HashMap<Character, ArrayList<Integer>> hashMap = new HashMap<>();
         for (int i = 0; i < text.length(); i++){
             char c = text.charAt(i);
-            positions = findAllOccurences(text, c);
+            positions = findAllOccurrences(text, c);
             hashMap.put(c, positions);
         }
         return hashMap;
